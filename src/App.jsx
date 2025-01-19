064ec85db98503df9174498c6930553a8990fbdc
Fasiha Rehmat Ali <fasihawebevis@gmail.com>
2025-01-20 00:38:15 +0500

    adding new section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Hero from './components/Hero'
 import Services from './components/Services'
+import Attributes from './components/Attributes'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Services/>
+      <Attributes/>
       <Projects/>
      
     </>
--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -5,16 +5,21 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 const Services = () => {
   const settings = {
-    dots:false,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    
-    // autoplay: true,
+    autoplay: true,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
         },
@@ -55,26 +60,34 @@ const Services = () => {
     },
   ];
   return (
-    <div className="bg-[url('/src/assets/services-bg.png')] bg-cover bg-center h-[500px] bg-gray-50 ">
-      <div className="container flex justify-between flex-col px-5 py-10 my-0 mx-auto lg:flex-row  ">
-        <div className="w-1/4  py-8 ">
-          <strong className="text-primary font-bold block mb-2">HOW WE WORKs</strong>
-          <h3 className="font-bold text-5xl mb-3">3 Easy Steps to Get Interior Design</h3>
+    <div className="bg-[url('/src/assets/services-bg.png')] bg-cover bg-center h-full bg-gray-50 ">
+      <div className=" flex justify-between flex-col px-5 py-10 my-0 mx-auto lg:flex-row  ">
+        <div className="lg:w-1/4  py-8 ">
+          <strong className="text-primary font-bold block mb-2">
+            HOW WE WORKs
+          </strong>
+          <h3 className="font-bold lg:text-5xl md:text-2xl mb-3">
+            3 Easy Steps to Get Interior Design
+          </h3>
           <p className="text-base">
             The of your kitchen varies from one layout to another. The shape of
             the kitchen also determines the space for cabinets, countertops, and
             accessories.
           </p>
         </div>
-        <div className="w-3/4 py-8 overflow-hidden">
+        <div className="lg:w-3/4 py-8 overflow-hidden">
           <Slider {...settings}>
             {sliderData.map((slide) => (
-              <div key={slide.id} className="p-4  w-80">
-                <div className="bg-white p-4 rounded-lg shadow-md h-80 flex flex-col items-start justify-center">
-                  <strong className="text-center text-7xl text-gray-300 block mb-5">0{slide.id}</strong>
-                  <strong className="block mb-5 text-xl font-medium">{slide.title}</strong>
-                  <p className="text-base mb-4">{slide.para}</p>
-                  <button className="transparent text-black">Read more</button>
+              <div key={slide.id} className="p-4 w-80">
+                <div className="bg-white min-h-96 h-full p-4 rounded-lg shadow-md  flex flex-col items-start justify-center">
+                  <strong className="text-center  text-7xl text-gray-300 block mb-5">
+                    0{slide.id}
+                  </strong>
+                  <strong className="block mb-5 w-52 lg:text-2xl md:text-xl font-medium">
+                    {slide.title}
+                  </strong>
+                  <p className="text-base mb-4 h-28">{slide.para}</p>
+                  <button className="transparent text-base text-black uppercase">Read more</button>
                 </div>
               </div>
             ))}
