1217700dab4728d18706e1d9cc9e38f27a3c8d02
Fasiha Rehmat Ali <fasihawebevis@gmail.com>
2025-01-20 00:51:49 +0500

    adding new section

--- src/components/Attributes/index.jsx
+++ src/components/Attributes/index.jsx
@@ -1,10 +1,37 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-
+import React from "react";
+import { SiAffinitydesigner } from "react-icons/si";
 const Attributes = () => {
+  const AttributeData = [
+    {
+      icon: <SiAffinitydesigner />,
+      title: "Custom Design",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, accusamus.",
+    },
+    {
+      icon: <SiAffinitydesigner />,
+      title: "Custom Design",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, accusamus.",
+    },
+    {
+      icon: <SiAffinitydesigner />,
+      title: "Custom Design",
+      para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, accusamus.",
+    },
+  ];
   return (
-    <div>Attributes</div>
-  )
-}
+    <div className="container m-10 ">
+    <div className="grid grid-cols-3 ">
+      {AttributeData.map((card,ind)=>(
+        <div key={ind}>
+            <div>{card.icon}</div>
+            <strong>{card.title}</strong>
+            <p>{card.para}</p>
+        </div>
+      ))}
+    </div>
+    </div>
+  );
+};
 
-export default Attributes
+export default Attributes;
